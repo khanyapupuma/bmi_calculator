c78ee717ebdd5bfccc916ae2eb197cb46dc64c12
khanyapupuma <khalilpupuma@gmail.com>
2024-07-13 20:57:49 +0200

    Update

--- script/code.js
+++ script/code.js
@@ -1,22 +1,18 @@
 function display(){
+    let  weight = document.querySelector('.p1').value
+    let height = document.querySelector('.p2').value
+    let output = document.querySelector('.ans')
 
-
-let  weight = document.querySelector('.p1')
-let height = document.querySelector('.p2')
-let output = document.querySelector('.output')
-
-
-let bmi = weight/height**2
-
-output.innerHTML = "Your BMI is equal to : "+ bmi.toFixed(2);
-if (bmi < 18.5 ){
-    output.innerHTML =  output.innerHTML + 'You are underweight'    
-}
-else if(bmi >= 18.5 && bmi<25){
-    output.innerHTML =  output.innerHTML + 'You have a normal weight'
-}
-else if(bmi >25 && bmi<30){
-    output.innerHTML =  output.innerHTML + 'You are overweight'
-}
-else output.innerHTML = output.innerHTML + 'You are obese'
-}
+    let bmi =  weight/height/height*10000
+    output.innerHTML = "Your BMI is equal to  " + bmi.toFixed(2);
+    if (bmi < 18.5 ){
+        output.innerHTML =  output.innerHTML + 'You are underweight'
+    }
+    else if(bmi >= 18.5 && bmi<25){
+        output.innerHTML =  output.innerHTML + 'You have a normal bmi'
+    }
+    else if(bmi >25 && bmi<30){
+        output.innerHTML =  output.innerHTML + 'You are overweight'
+    }
+    else output.innerHTML = output.innerHTML + 'You are obese'
+    }
